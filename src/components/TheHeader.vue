f017ead6a0c8fde27d5b5a72194e34fb5cecd71f
Laura-Klery <lklery@yahoo.fr>
2023-07-11 11:41:52 +0200

    Mise en place du responsive sur le header
    // TODO :
    - Mise en place des composants

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,81 +1,35 @@
-<script>
-export default {
-  name: "TheHeader",
-  data() {
-    return {
-      isResponsive: false,
-      activeLink: null,
-    };
-  },
-  methods: {
-    toggle() {
-      this.isResponsive = !this.isResponsive;
-    },
-    closeMenu() {
-      this.isResponsive = !this.isResponsive;
-    },
-    setActiveLink: function (event) {
-      this.closeMenu();
-      event.preventDefault();
-      var link = event.target;
-      var sectionId = link.getAttribute("href");
-      var section = document.querySelector(sectionId);
-      if (this.activeLink) {
-        this.activeLink.classList.remove("active");
-      }
-      link.classList.add("active");
-      this.activeLink = link;
-      section.scrollIntoView({ behavior: "smooth" });
-    },
-  },
-};
+<script setup>
+import {ref} from "vue";
+
+const isNavOpen = ref(false)
+
+const toggleNav = () => {
+  isNavOpen.value = !isNavOpen.value
+}
+
 </script>
 
 <template>
   <header class="main-header">
-    <nav
-      id="topnav"
-      class="nav"
-      :class="{ topnav: true, responsive: isResponsive }"
-    >
-      <div class="container-logo">
-        <router-link to="/" class="logo"><img src="@/assets/img/logo.svg" alt="Logo du site constitué de la lettre l et de la lettre k entouré d'accolade." /></router-link>
-      </div>
-      <div>
-        <div class="container-links">
-          <a class="link" href="#about" @click="closeMenu; setActiveLink($event)">
-            Présentation
+    <nav id="topnav" class="nav" :class="{ 'nav-open': isNavOpen }">
+      <div class="logo"><router-link to="/"><img src="@/assets/img/logo.svg" alt="Logo du site constitué de la lettre l et de la lettre k entouré d'accolade." /></router-link></div>
+      <div class="menu">
+        <div class="menu-icon" @click="toggleNav" :class="{ 'cross': isNavOpen }">
+          <span class="line1"></span>
+          <span class="line2"></span>
+          <span class="line3"></span>
+        </div>
+        <ul>
+          <li><a href="#about">Présentation</a></li>
+          <li><a href="#skills">Compétences</a></li>
+          <li><a href="#formations">Expériences</a></li>
+          <li><a href="#projects">Projets</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <div class="social-icons">
+          <a href="https://www.linkedin.com/in/laura-klerykowski/" target="_blank" rel="noopener noreferrer">
+            <img src="@/assets/icons/linkedin.svg" alt="LinkedIn" />
           </a>
-          <a class="link" href="#skills" @click="closeMenu; setActiveLink($event)">
-            Compétences
-          </a>
-          <a
-            class="link"
-            href="#formations"
-            @click="closeMenu; setActiveLink($event)"
-          >
-            Expériences
-          </a>
-          <a
-            class="link"
-            href="#projects"
-            @click="closeMenu; setActiveLink($event)"
-          >
-            Projets
-          </a>
-          <a
-            class="link"
-            href="#contact"
-            @click="closeMenu; setActiveLink($event)"
-          >
-            Contact
-          </a>
-          <a href="https://www.linkedin.com/in/laura-klerykowski/" class="icon-container">
-            <img src="@/assets/icons/linkedin.svg" alt="Lien Linkedin" />
-          </a>
-          <button class="icon" @click="toggle">
-            <img src="@/assets/icons/menu.svg" alt="Icône du menu burger" />
-          </button>
         </div>
       </div>
     </nav>
